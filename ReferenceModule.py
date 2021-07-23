ebd71ab5a8b083309ff012abfc15da65ba3463db
fabyanMikhael <fabyanmikhael@yahoo.ca>
2021-07-22 21:56:27 -0400

    updated it to work with AutoJson

--- ReferenceModule.py
+++ ReferenceModule.py
@@ -10,28 +10,40 @@ class __tmp__:
 
 class Reference(Generic[U]):
     TYPE : U = __tmp__
-    REFERENCES : dict[str,Reference] = {}
-    def __init__(self, id) -> None:
+    REFERENCES : dict[str,Reference]
+    __ALLOWED_TO_ACCESS__ = ["__referencing__", "__id__", "TYPE", "__Accessed_Attribute__", "REFERENCES", "ToDict", "FromDict", "__dict__", "__eq__"]
+    __ALLOWED_TO_MODIFY__ = ["__referencing__", "__id__", "TYPE", "__Accessed_Attribute__", "REFERENCES", "ToDict", "__dict__"]
+
+    def __init_subclass__(cls, scm_type=None, name=None, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls.REFERENCES = {}
+
+    def __init__(self, __id__) -> None:
+        __id__ = str(__id__)
         self.__referencing__ = None
-        self.__id__ = id
-        self.REFERENCES[id] = self
+        self.__id__ = __id__
+        self.REFERENCES[__id__] = self
 
     def __repr__(self) -> str:
         self.__Accessed_Attribute__()
         return self.__referencing__.__repr__()
+    
+    def __eq__(self, o: object) -> bool:
+        self.__Accessed_Attribute__()
+        return self.__referencing__ == o
 
     def __str__(self) -> str:
         self.__Accessed_Attribute__()
         return self.__referencing__.__str__()
 
     def __getattribute__(self, name: str):
-        if name in ["__referencing__", "__id__", "TYPE", "__Accessed_Attribute__", "REFERENCES", "__clear__"]:
+        if name in Reference.__ALLOWED_TO_ACCESS__:
             return object.__getattribute__(self, name)
         self.__Accessed_Attribute__()
         return object.__getattribute__(self.__referencing__, name)
     
     def __setattr__(self, name: str, value) -> None:
-        if name in ["__referencing__", "__id__", "TYPE"]:
+        if name in Reference.__ALLOWED_TO_MODIFY__:
             object.__setattr__(self, name, value)
             return
         self.__Accessed_Attribute__()
@@ -48,7 +60,14 @@ class Reference(Generic[U]):
             ref.__referencing__ = None
 
     @classmethod
+    def pop(cls, id) -> None:
+        id = str(id)
+        if id in cls.REFERENCES:
+            cls.REFERENCES[id].__referencing__ = None
+
+    @classmethod
     def Get(cls, id) -> U:
+        id = str(id)
         if id in cls.REFERENCES:
             return cls.REFERENCES[id]
-        return cls(id=id)
+        return cls(__id__=id)
